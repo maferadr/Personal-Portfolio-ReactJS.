86baea8c9f189e968412ddd0267863520c7ebfff
maferadr <maferadr0604@gmail.com>
2024-04-27 18:57:01 -0400

    testing2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage(){
             <div className="img-container grid h-screen">
                 <img className="col-start-1 row-start-1 w-100 h-[80vh] object-cover scale-y-[-1] opacity-70" src="/assets/orange.png"></img>
                 <img className="col-start-1 row-start-1 my-60 w-30 h-[60vh] object-cover scale-x-[-1] opacity-60" src="/assets/blue-powder.png"></img>
-                <img className="myself-main col-start-1 row-start-1 w-100 h-[80vh] object-cover object-left-bottom bottom-0" src="/assets/me-main.png"></img>
+                <img className="myself-main col-start-1 row-start-1 object-cover object-left-bottom bottom-0" src="/assets/me-main.png"></img>
                 <div id="main-text" className="absolute justify-center my-[10%] ml-[55%]">
                     <h1><span id="title-1">FULL</span> <br></br><span id="title-2">Stack</span>
                     <br></br><span id="title-3">
