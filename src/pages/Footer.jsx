c576779e8836996bdd3d8142ce2a5a7bc0c88983
maferadr <maferadr0604@gmail.com>
2024-08-14 18:08:20 -0400

    footer fixed

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,16 +4,14 @@ import { BiLogoGmail } from "react-icons/bi";
 
 export default function Footer(){
     return(
-        <div className="bg-white w-100 h-60 footer-container">
-        <div className="container wrap-footer bottom" data-aos='fade-up-left' data-aos-delay="100">
-            <h1 className="py-5 flex justify-center bg-orange-50 contact-me" id="resume-btn">CONTACT ME</h1>
-            <p className="opacity-70 flex justify-center" data-aos='fade-up-right' data-aos-duration='1000'>Let's keep up the good work together!</p>
+      <div className="h-60 bg-orange-50 p-5">
+            <h1 className="flex justify-center contact pt-5 pb-3" data-aos='fade-up-left' data-aos-delay="100">Contact Me</h1>
+            <p className="text-contact flex justify-center">Let's keep up the good work together!</p>
             <div className="flex justify-center" data-aos='fade-left' data-aos-duration='1500'>
                     <span className="icon"><a href="https://www.linkedin.com/in/maria-angulo-5249552b5/"><AiFillLinkedin/></a></span>
                     <span className="icon"><a href="https://github.com/maferadr"><AiFillGithub/></a></span>
                     <span className="icon"><a href="mailto:maferadr0604@gmail.com"><BiLogoGmail/></a></span>
             </div>
-        </div>
-        </div>
+      </div>
     )
 }
